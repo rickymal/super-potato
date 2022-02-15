600cc442207887e1fb6efb3889808719480344e5
Henrique Borges <henriquem@speediomail.com>
2022-02-14 21:58:22 -0300

    primeiro commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/login">login</RouterLink>
+  </nav>
+  <RouterView/>
+</template>
+
+<style>
+@import "@/assets/base.css";
+@import "@/bootstrap/css/bootstrap.css";
+#app {
+  margin: 0 5px;
+  height : 100vh;
+  width : 100vw;
+  padding: 0 5px;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ front-end/src/views/FormLoginView.vue
@@ -0,0 +1,88 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+
+<script>
+export default {
+  methods : {
+    make_submition(event) {
+      event.preventDefault()
+
+      console.log('iniciando a submissão','oi')
+      const email = this.email_description
+      const password = this.password_description
+
+      let myHeaders = new Headers();
+
+
+      var content_to_send = JSON.stringify({
+        email, password
+      });
+        
+
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("content-type", "application/json");
+      myHeaders.append("Content-Length", content_to_send.length.toString());
+      myHeaders.append("X-Custom-Header", "ProcessThisImmediately");
+
+      
+      let myInit = { 
+        method: 'POST',
+        headers: myHeaders,
+        mode: 'no-cors',
+        body: content_to_send,
+        cache: 'default' 
+      };
+      
+
+      let myRequest = new Request('http://localhost:3003/login', myInit);
+      fetch(myRequest)
+        .then(function(response) {
+          var contentType = response.headers.get("content-type");
+          if(contentType && contentType.indexOf("application/json") !== -1) {
+            return response.json().then((json) => {
+              
+            });
+          } else {
+
+          }
+        });
+    }
+  }
+}
+
+</script>
+
+
+<template>
+  <main>
+    <form class="frm-login">
+      <div class="mb-3">
+        <label for="input_email_01" class="form-label">Endereço de e-mail</label>
+        <input type="email" class="form-control" id="input_email_01" aria-describedby="email_description" :value="input_email" @input="event => email_description = event.target.value">
+        <div id="email_description" class="form-text">A senha não será compartilhada, não se preocupe.</div>
+      </div>
+      <div class="mb-3">
+        <label for="input_password_01" class="form-label">Password</label>
+        <input type="password" class="form-control" id="input_password_01" :value="input_password" @input="event => password_description = event.target.value">
+      </div>
+      <!-- <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="input_checkbox_01">
+        <label class="form-check-label" for="input_checkbox_01">Login automático</label>
+      </div> -->
+      <button @click = "make_submition" type="submit" class="btn btn-primary">Realizar login</button>
+    </form>
+  </main>
+</template>
+
+
+<style>
+
+form.frm-login {
+  padding: 0 10rem;
+}
+
+
+
+</style>
