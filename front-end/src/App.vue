5ef59963a3580c39238c26e1e0b7e1453703c084
Henrique Borges <henriquem@speediomail.com>
2022-02-23 20:08:00 -0300

    Fechamento de tag

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 <template>
+  <RouterView>
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
